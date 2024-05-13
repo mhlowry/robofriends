4549745f9021434cfa0631104ee2fb9d950bb4b3
Michael Lowry <michael.lowry64@gmail.com>
2024-05-13 15:05:01 -0400

    Search added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './robots';
@@ -9,7 +9,7 @@ import { robots } from './robots';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <CardList robots={robots}/>
+      <App />
   </React.StrictMode>
 );
 
